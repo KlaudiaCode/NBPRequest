3fd5d3029a77b46c0760ece0d58da4b6f9fdc913
Klaudia <oposyyy@gmail.com>
2020-04-19 11:17:59 +0200

    Update models, form and added display & validation.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace NBPRequest
 {
-    partial class Form1
+    partial class FormForCurrency
     {
         /// <summary>
         /// Wymagana zmienna projektanta.
@@ -28,47 +28,262 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxWindow = new System.Windows.Forms.TextBox();
+            this.buttonTable = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.radioButtonA = new System.Windows.Forms.RadioButton();
+            this.radioButtonB = new System.Windows.Forms.RadioButton();
+            this.radioButtonC = new System.Windows.Forms.RadioButton();
+            this.comboBoxCash = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // textBoxWindow
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 26);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(330, 393);
-            this.textBox1.TabIndex = 0;
+            this.textBoxWindow.Location = new System.Drawing.Point(274, 7);
+            this.textBoxWindow.Multiline = true;
+            this.textBoxWindow.Name = "textBoxWindow";
+            this.textBoxWindow.Size = new System.Drawing.Size(321, 225);
+            this.textBoxWindow.TabIndex = 0;
             // 
-            // button1
+            // buttonTable
             // 
-            this.button1.Location = new System.Drawing.Point(267, 425);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Download";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonTable.Location = new System.Drawing.Point(121, 94);
+            this.buttonTable.Name = "buttonTable";
+            this.buttonTable.Size = new System.Drawing.Size(121, 28);
+            this.buttonTable.TabIndex = 1;
+            this.buttonTable.Text = "Download";
+            this.buttonTable.UseVisualStyleBackColor = true;
+            this.buttonTable.Click += new System.EventHandler(this.buttonTable_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 7);
+            this.label1.Location = new System.Drawing.Point(12, 19);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(220, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Click button below to download current rates.";
             // 
-            // Form1
+            // radioButtonA
+            // 
+            this.radioButtonA.AutoSize = true;
+            this.radioButtonA.Checked = true;
+            this.radioButtonA.Location = new System.Drawing.Point(16, 59);
+            this.radioButtonA.Name = "radioButtonA";
+            this.radioButtonA.Size = new System.Drawing.Size(62, 17);
+            this.radioButtonA.TabIndex = 3;
+            this.radioButtonA.TabStop = true;
+            this.radioButtonA.Text = "Table A";
+            this.radioButtonA.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonB
+            // 
+            this.radioButtonB.AutoSize = true;
+            this.radioButtonB.Location = new System.Drawing.Point(16, 82);
+            this.radioButtonB.Name = "radioButtonB";
+            this.radioButtonB.Size = new System.Drawing.Size(62, 17);
+            this.radioButtonB.TabIndex = 4;
+            this.radioButtonB.TabStop = true;
+            this.radioButtonB.Text = "Table B";
+            this.radioButtonB.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonC
+            // 
+            this.radioButtonC.AutoSize = true;
+            this.radioButtonC.Location = new System.Drawing.Point(16, 105);
+            this.radioButtonC.Name = "radioButtonC";
+            this.radioButtonC.Size = new System.Drawing.Size(62, 17);
+            this.radioButtonC.TabIndex = 5;
+            this.radioButtonC.TabStop = true;
+            this.radioButtonC.Text = "Table C";
+            this.radioButtonC.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxCash
+            // 
+            this.comboBoxCash.FormattingEnabled = true;
+            this.comboBoxCash.Items.AddRange(new object[] {
+            "<- - - -TABLE- - - ->",
+            "AED",
+            "AFN",
+            "ALL",
+            "AMD",
+            "ANG",
+            "AOA",
+            "ARS",
+            "AUD",
+            "AWG",
+            "AZN",
+            "BAM",
+            "BBD",
+            "BDT",
+            "BGN",
+            "BHD",
+            "BIF",
+            "BMD",
+            "BND",
+            "BOB",
+            "BRL",
+            "BSD",
+            "BTN",
+            "BWP",
+            "BYN",
+            "BZD",
+            "CAD",
+            "CDF",
+            "CHF",
+            "CLP",
+            "CNY",
+            "COP",
+            "CRC",
+            "CUC",
+            "CUP",
+            "CVE",
+            "CZK",
+            "DJF",
+            "DKK",
+            "DOP",
+            "DZD",
+            "EGP",
+            "ERN",
+            "ETB",
+            "EUR",
+            "FJD",
+            "FKP",
+            "GBP",
+            "GEL",
+            "GGP",
+            "GHS",
+            "GIP",
+            "GMD",
+            "GNF",
+            "GTQ",
+            "GYD",
+            "HKD",
+            "HNL",
+            "HRK",
+            "HTG",
+            "HUF",
+            "IDR",
+            "ILS",
+            "IMP",
+            "INR",
+            "IQD",
+            "IRR",
+            "ISK",
+            "JEP",
+            "JMD",
+            "JOD",
+            "JPY",
+            "KES",
+            "KGS",
+            "KHR",
+            "KMF",
+            "KPW",
+            "KRW",
+            "KWD",
+            "KYD",
+            "KZT",
+            "LAK",
+            "LBP",
+            "LKR",
+            "LRD",
+            "LSL",
+            "LYD",
+            "MAD",
+            "MDL",
+            "MGA",
+            "MKD",
+            "MMK",
+            "MNT",
+            "MOP",
+            "MRU",
+            "MUR",
+            "MVR",
+            "MWK",
+            "MXN",
+            "MYR",
+            "MZN",
+            "NAD",
+            "NGN",
+            "NIO",
+            "NOK",
+            "NPR",
+            "NZD",
+            "OMR",
+            "PAB",
+            "PEN",
+            "PGK",
+            "PHP",
+            "PKR",
+            "PLN",
+            "PYG",
+            "QAR",
+            "RON",
+            "RSD",
+            "RUB",
+            "RWF",
+            "SAR",
+            "SBD",
+            "SCR",
+            "SDG",
+            "SEK",
+            "SGD",
+            "SHP",
+            "SLL",
+            "SOS",
+            "SPL",
+            "SRD",
+            "STN",
+            "SVC",
+            "SYP",
+            "SZL",
+            "THB",
+            "TJS",
+            "TMT",
+            "TND",
+            "TOP",
+            "TRY",
+            "TTD",
+            "TVD",
+            "TWD",
+            "TZS",
+            "UAH",
+            "UGX",
+            "USD",
+            "UYU",
+            "UZS",
+            "VEF",
+            "VND",
+            "VUV",
+            "WST",
+            "XAF",
+            "XCD",
+            "XDR",
+            "XOF",
+            "XPF",
+            "YER",
+            "ZAR",
+            "ZMW",
+            "ZWD"});
+            this.comboBoxCash.Location = new System.Drawing.Point(121, 55);
+            this.comboBoxCash.Name = "comboBoxCash";
+            this.comboBoxCash.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxCash.TabIndex = 0;
+            this.comboBoxCash.Text = "<- - - -TABLE- - - ->";
+            // 
+            // FormForCurrency
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(364, 459);
+            this.ClientSize = new System.Drawing.Size(612, 252);
+            this.Controls.Add(this.comboBoxCash);
+            this.Controls.Add(this.radioButtonC);
+            this.Controls.Add(this.radioButtonB);
+            this.Controls.Add(this.radioButtonA);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Name = "Form1";
+            this.Controls.Add(this.buttonTable);
+            this.Controls.Add(this.textBoxWindow);
+            this.Name = "FormForCurrency";
             this.Text = "Currency rates";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -77,9 +292,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxWindow;
+        private System.Windows.Forms.Button buttonTable;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButtonA;
+        private System.Windows.Forms.RadioButton radioButtonB;
+        private System.Windows.Forms.RadioButton radioButtonC;
+        private System.Windows.Forms.ComboBox comboBoxCash;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,29 +13,33 @@ using Newtonsoft.Json;
 
 namespace NBPRequest
 {
-    public partial class Form1 : Form
+    public partial class FormForCurrency : Form
     {
-        public Form1()
+        public FormForCurrency()
         {
             InitializeComponent();
         }
-
-        public class RateForTable
+        /// <summary>
+        /// TABLES MODELS
+        /// </summary>
+        public class RateForTableAB
         {
             public string Currency { get; set; }
             public string Code { get; set; }
             public double Mid { get; set; }
             public override string ToString()
             {
-                return this.Currency + " " + this.Code + " " + this.Mid + " ";
+                return "Currency: " + this.Currency + Environment.NewLine + 
+                       "Code: " + this.Code + Environment.NewLine +
+                       "Mid: " + this.Mid + Environment.NewLine;
             }
         }
-        public class RequestForTable
+        public class RequestForTableAB
         {
             public string Table { get; set; }
             public string No { get; set; }
             public string EffectiveDate { get; set; }
-            public List<RateForTable> Rates { get; set; }
+            public List<RateForTableAB> Rates { get; set; }
             public override string ToString()
             {
                 string ratesList = "";
@@ -45,40 +49,239 @@ namespace NBPRequest
                     ratesList += this.Rates[i] + Environment.NewLine;
                 }
 
-                return "Table: " + this.Table + "    No: " + this.No + "    Effective date: " + this.EffectiveDate + Environment.NewLine + Environment.NewLine + ratesList;
+                return "Table: " + this.Table + Environment.NewLine +
+                       "No: " + this.No + Environment.NewLine +
+                       "Effective date: " + this.EffectiveDate + Environment.NewLine + Environment.NewLine + 
+                       ratesList;
             }
         }
 
-        public class RateForCurrency
+        public class RateForTableC
+        {
+            public string Currency { get; set; }
+            public string Code { get; set; }
+            public double Bid { get; set; }
+            public double Ask { get; set; }
+            public override string ToString()
+            {
+                return "Currency: " + this.Currency + Environment.NewLine +
+                       "Code: " + this.Code + Environment.NewLine +
+                       "Bid: " + this.Bid + Environment.NewLine +
+                       "Ask: " + this.Ask + Environment.NewLine;
+            }
+        }
+        public class RequestForTableC
+        {
+            public string Table { get; set; }
+            public string No { get; set; }
+            public string TradingDate { get; set; }
+            public string EffectiveDate { get; set; }
+            public List<RateForTableC> Rates { get; set; }
+            public override string ToString()
+            {
+                string ratesList = "";
+
+                for (int i = 0; i < Rates.LongCount(); i++)
+                {
+                    ratesList += this.Rates[i] + Environment.NewLine;
+                }
+
+                return "Table: " + this.Table + Environment.NewLine +
+                       "No: " + this.No + Environment.NewLine +
+                       "TradingDate: " + this.TradingDate + Environment.NewLine +
+                       "Effective date: " + this.EffectiveDate + Environment.NewLine + Environment.NewLine + 
+                       ratesList;
+            }
+        }
+
+        /// <summary>
+        /// CURRENCY MODELS
+        /// </summary>
+
+        public class RateForCurrencyAB
         {
             public string No { get; set; }
             public string EffectiveDate { get; set; }
             public double Mid { get; set; }
+            public override string ToString()
+            {
+                return "No: " + this.No + Environment.NewLine + 
+                       "Effective date: " +  this.EffectiveDate + Environment.NewLine +
+                       "Mid: " + this.Mid;
+            }
         }
-        public class RequestForCurrency
+
+        public class RateForCurrencyC
+        {
+            public string No { get; set; }
+            public string EffectiveDate { get; set; }
+            public double Bid { get; set; }
+            public double Ask { get; set; }
+
+            public override string ToString()
+            {
+                return "No: " + this.No + Environment.NewLine +
+                       "Effective date: " + this.EffectiveDate + Environment.NewLine +
+                       "Bid: " + this.Bid + Environment.NewLine +
+                       "Ask: " + this.Ask + Environment.NewLine;
+
+            }
+        }
+        public class RequestForCurrencyA
         {
             public string Table { get; set; }
             public string Currency { get; set; }
             public string Code { get; set; }
-            public List<RateForCurrency> Rates { get; set; }
+            public List<RateForCurrencyAB> Rates { get; set; }
+            public override string ToString()
+            {
+                string ratesList = " ";
+                for (int i = 0; i < Rates.LongCount(); i++)
+                {
+                    ratesList += Rates[i];
+                }
+                 
+                return "Table: " + this.Table + Environment.NewLine +
+                       "Currency: " + this.Currency + Environment.NewLine +
+                       "Code: " + this.Code + Environment.NewLine + Environment.NewLine + 
+                       ratesList;
+            }
         }
 
+        public class RequestForCurrencyB
+        {
+            public string Table { get; set; }
+            public string Country { get; set; }
+            public double Symbol { get; set; }
+            public string Currency { get; set; }
+            public string Code { get; set; }
+            public List<RateForCurrencyAB> Rates { get; set; }
+            public override string ToString()
+            {
+                string ratesList = " ";
+                for (int i = 0; i < Rates.LongCount(); i++)
+                {
+                    ratesList += Rates[i];
+                }
+
+                return "Table: " + this.Table + Environment.NewLine +
+                       "Country: " + this.Country + Environment.NewLine +
+                       "Symbol: " + this.Symbol + Environment.NewLine +
+                       "Currency: " + this.Currency + Environment.NewLine +
+                       "Code: " + this.Code + Environment.NewLine + Environment.NewLine +
+                       ratesList;
+            }
+        }
+
+        public class RequestForCurrencyC
+        {
+            public string Table { get; set; }
+            public string Currency { get; set; }
+            public string Code { get; set; }
+            public List<RateForCurrencyC> Rates { get; set; }
+            public override string ToString()
+            {
+                string ratesList = " ";
+                for (int i = 0; i < Rates.LongCount(); i++)
+                {
+                    ratesList += Rates[i];
+                }
+
+                return "Table: " + this.Table + Environment.NewLine +
+                       "Currency: " + this.Currency + Environment.NewLine +
+                       "Code: " + this.Code + Environment.NewLine + Environment.NewLine +
+                       ratesList;
+            }
+        }
+        /// <summary>
+        /// /////////////////////////
+        /// </summary>
         public static class Helper
         {
-            public static string client()
+            public static string Client(string letter)
             {
-            WebClient webClient = new WebClient();
-            return webClient.DownloadString("https://api.nbp.pl/api/exchangerates/tables/A/?format=json");
+
+                WebClient webClient = new WebClient();
+
+                string website = "https://api.nbp.pl/api/exchangerates/tables/" + letter + "/?format=json";
+                return webClient.DownloadString(website);
+
             }
-            
+            public static string Client(string letter, string code)
+            {
+
+                WebClient webClient = new WebClient();
+
+                string website = " https://api.nbp.pl/api/exchangerates/rates/" + letter + "/" + code + "/" + "/?format=json";
+                return webClient.DownloadString(website);
+
+            }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void buttonTable_Click(object sender, EventArgs e)
         {
-            textBox1.ScrollBars = ScrollBars.Both;
-            textBox1.WordWrap = false;
-            RequestForTable[] rft = JsonConvert.DeserializeObject<RequestForTable[]>(Helper.client());
-            textBox1.Text = rft[0].ToString();
+            string letter = "A";
+            string code = comboBoxCash.Text;
+
+            if (radioButtonB.Checked == true)
+            {
+                letter = "B";
+            }
+            else if (radioButtonC.Checked == true)
+            {
+                letter = "C"; 
+            }
+
+
+            if (letter == "A" && code == "<- - - -TABLE- - - ->" || letter == "B" && code == "<- - - -TABLE- - - ->")
+            {
+                RequestForTableAB[] rft = JsonConvert.DeserializeObject<RequestForTableAB[]>(Helper.Client(letter));
+                textBoxWindow.Text = rft[0].ToString();
+            }
+            else if (letter == "C" && code == "<- - - -TABLE- - - ->")
+            {
+                RequestForTableC[] rft = JsonConvert.DeserializeObject<RequestForTableC[]>(Helper.Client(letter));
+                textBoxWindow.Text = rft[0].ToString();
+            }
+            else if (letter == "A")
+            {
+                try
+                {
+                    RequestForCurrencyA rfc = JsonConvert.DeserializeObject<RequestForCurrencyA>(Helper.Client(letter, code));
+                    textBoxWindow.Text = rfc.ToString();
+                }
+                catch (Exception)
+                {
+                    textBoxWindow.Text = "There is no such currency in this table." + Environment.NewLine + "Please make sure you choose the right currency/table.";
+                }
+            }
+            else if (letter == "B")
+            {
+                try
+                {
+                    RequestForCurrencyB rfc = JsonConvert.DeserializeObject<RequestForCurrencyB>(Helper.Client(letter, code));
+                    textBoxWindow.Text = rfc.ToString();
+                }
+                catch (Exception)
+                {
+                    textBoxWindow.Text = "There is no such currency in this table." + Environment.NewLine + "Please make sure you choose the right currency/table.";
+                }
+            }
+            else if (letter == "C")
+            { 
+                try
+                {
+                    RequestForCurrencyC rfc = JsonConvert.DeserializeObject<RequestForCurrencyC>(Helper.Client(letter, code));
+                    textBoxWindow.Text = rfc.ToString();
+                }
+                catch (Exception)
+                {
+                    textBoxWindow.Text = "There is no such currency in this table." + Environment.NewLine + "Please make sure you choose the right currency/table.";
+                }
+            }
+
+            textBoxWindow.ScrollBars = ScrollBars.Both;
+            textBoxWindow.WordWrap = false;
         }
     }
 }
